29eb8de5b0c8d6d48b663580a6a62e556df3d10b
Mohamed Sheikhwali <mohamedsheikhwali@gmail.com>
2023-01-28 13:56:07 -0800

    updated html page

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  // we get the form element
+  const form = document.querySelector("form");
+  // we create an event handler for the form's submission
+  form.onsubmit = function(event) }
