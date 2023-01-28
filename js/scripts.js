c196b95fefb61a9c07ccf82c3be0698552b4d9f3
Mohamed Sheikhwali <mohamedsheikhwali@gmail.com>
2023-01-27 17:01:58 -0800

    Added Question list in html, updated javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 
 const form = document.getElementById('subscribe');
 
-<button type="submit">Subscribe</button>
+<button type="submit">subscribe</button>
 
+const form = document.getElementById('signup');
+
+form.addEventListener('submit', (Event) => {
+// handle the form data
+});
